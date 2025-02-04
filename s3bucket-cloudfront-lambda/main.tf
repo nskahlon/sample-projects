0ab81f1bb1433f771da8fced1f9d8d7cff0fa5b2
navneet Kahlon <navneetkahlon@outlook.com>
2025-02-03 22:04:02 -0500

    added version reference for modules

--- s3bucket-cloudfront-lambda/main.tf
+++ s3bucket-cloudfront-lambda/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "intact_s3_bucket" {
   //source      = "./modules/s3bucket"
-  source = "github.com/nskahlon/sample-projects/s3bucket-cloudfront-lambda/modules/s3bucket"
+  source = "github.com/nskahlon/sample-projects/s3bucket-cloudfront-lambda/modules/s3bucket?ref=v1.0.0-alpha"
   bucket_name = "project-images-nkahlon"
   tags = {
     "Owner"     = "Navneet Kahlon"
@@ -14,7 +14,7 @@ module "intact_s3_bucket" {
 }
 module "intact_cf_distribution" {
   depends_on         = [module.intact_s3_bucket]
-  source             = "github.com/nskahlon/sample-projects/s3bucket-cloudfront-lambda/modules/cloudfront"
+  source             = "github.com/nskahlon/sample-projects/s3bucket-cloudfront-lambda/modules/cloudfront?ref=v1.0.0-alpha"
   s3_regional_domain = module.intact_s3_bucket.regional_domain
   default_acm_cert_cf_distribution = "arn:aws:acm:us-east-1:359196653330:certificate/813a3bea-a8c3-4b04-8d57-6a442874a144"
   cf_aliases         = ["dev-project-images.cs.idc.com", "dev-project-images.idc-cs-assessment.com"]
